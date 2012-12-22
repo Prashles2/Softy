f68de097402f827db11f98e0205fc108acb59df4
Prash <prash.somaiya@gmail.com>
2012-12-22 15:23:20 +0000

    inital folder structure

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+* Sample file - all exmaples
+*
+* Index file 
+*/
+
+define('BASE_PATH', dirname(__FILE__));
+
+define('CORE_PATH', BASE_PATH.'/../softy/core/');
+
+define('APP_PATH', BASE_PATH.'/../softy/app/');
